52e4e16a72d8518ef7fc62d2a9eb6b65ff2497fe
Umut <umtgokdemir@gmail.com>
2024-04-18 18:15:11 +0200

    Binary search algorithm implemented.

--- search_algorithms.py
+++ search_algorithms.py
@@ -26,3 +26,37 @@ class SearchAlgorithms:
             return None
         except Exception as e:
             print(f"An error occurred. Please check the inputs again. Error : {e}")
+
+    # Define linear search algorithm to return list of repeated search values in the given list
+    def LinearSearchList(self):
+        try:
+            return [index for index in range(self.length) if self.searching_list[index] == self.searching_item]
+        except Exception as e:
+            print("Error")
+
+            # Define binary search algorithm
+    def BinarySearch(self):
+        try:
+            # Check the given list is sorted or not
+            if not sorted(self.searching_list) == self.searching_list:
+                print("Given list is not sorted. To perform Binary search, it is sorted.")
+                self.searching_list.sort()
+                left,right = 0,self.length-1
+
+            while left<=right:
+                # Decide middlie point of the looking section of the sorted list
+                searching_position = (left+right)//2
+
+                # Check middle element of the list if it is searching item or not
+                if self.searching_item == self.searching_list[searching_position]:
+                    return (self.searching_list,searching_position)
+
+                    # Check searching item if it is near to the left or not
+                elif self.searching_item < self.searching_list[searching_position]:
+                    right = searching_position - 1
+                else:
+                    left = searching_position + 1
+
+        except ValueError:
+            print(f"{self.searching_item} is not in the list.")
+            return None
